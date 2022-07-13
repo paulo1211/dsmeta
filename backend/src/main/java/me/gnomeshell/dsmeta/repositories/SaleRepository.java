8e9606bf631fc31a043d1bc9d8bf7eed54547eb2
Paulo Ricardo <pauloaraujo1211@outlook.com>
2022-07-13 18:57:05 -0300

    API test

---
+++ backend/src/main/java/me/gnomeshell/dsmeta/repositories/SaleRepository.java
@@ -0,0 +1,8 @@
+package me.gnomeshell.dsmeta.repositories;
+
+import me.gnomeshell.dsmeta.entities.Sale;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface SaleRepository extends JpaRepository<Sale, Long> {
+
+}
