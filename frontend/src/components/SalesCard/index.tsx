a8566cc6ee3c445a791075c9a493ca0d2cd9bfb1
Paulo Ricardo <pauloaraujo1211@outlook.com>
2022-07-15 14:30:25 -0300

    Axios, useEffect first request

--- frontend/src/components/SalesCard/index.tsx
+++ frontend/src/components/SalesCard/index.tsx
@@ -1,8 +1,9 @@
 import NotificationButton from '../NotificationButton'
 import './styles.css'
+import axios from "axios";
 import DatePicker from "react-datepicker";
 import "react-datepicker/dist/react-datepicker.css";
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 
 function SalesCard(){
 
@@ -12,6 +13,14 @@ function SalesCard(){
     const [minDate, setMinDate] = useState(min);
     const [maxDate, setMaxDate] = useState(max);
    
+    useEffect(() => {
+        axios.get("https://dsmeta-paulo.herokuapp.com/sales")
+        .then(response => {
+            console.log(response.data);
+        })
+    }, []);
+
+
     return (
         <div className="dsmeta-card">
             <h2 className="dsmeta-sales-title">Vendas</h2>
@@ -98,7 +107,6 @@ function SalesCard(){
             </table>
 
         </div>
-
 
     </div>
     )
